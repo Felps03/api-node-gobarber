6d4d2eef66dbbbe5d20eeed28c9549d8cee20e06
Felps03 <felipe.031293@gmail.com>
2019-10-14 22:11:08 -0300

    feat(migrations): create new migrations

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware  from './app/middlewares/auth';
 
@@ -19,5 +21,10 @@ routes.use(authMiddleware);
 routes.put('/user', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/provider', ProviderController.index);
+
+
+routes.post('/appointment', AppointmentController.store);
 
 export default routes;
