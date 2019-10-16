f647c5b7de1d469366692830c89499f5b38abe27
Felps <felipe.031293@gmail.com>
2019-10-16 10:31:44 -0300

    mail and add redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,8 @@ import Appointment from '../models/Appointment'
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
 
@@ -112,10 +113,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento'
+    await Queue.add(CancellationMail.key, {
+      appointment
     });
 
     return res.json(appointment);
