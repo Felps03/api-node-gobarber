6bdf9d6da681fe0b846818369774adbb4431c5fd
Felps <felipe.031293@gmail.com>
2019-10-16 09:51:18 -0300

    email import

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,7 +5,9 @@ import locale from 'date-fns/locale/pt';
 import User from '../models/User';
 import Appointment from '../models/Appointment'
 import File from '../models/File';
-import Notification from '../schemas/Notification'
+import Notification from '../schemas/Notification';
+
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
 
@@ -88,7 +90,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email']
+        }
+      ]
+    });
 
     if (appointment.user_id != req.userId)
       return res.status(401).json({ error: "You don't have permission to cancel this appointment" });
@@ -101,6 +111,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento'
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7ed01a146a53e6',
+    pass: '4d548cd8afe7c4'
+  },
+  default: {
+    from: 'Programmer <felipe.031293@gmail.com>'
+  }
+}
