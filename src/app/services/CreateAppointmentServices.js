e7ec827c39a5da0c3f1804a2fbfcae4d26cb70b1
Felps <felipe.031293@gmail.com>
2019-10-17 11:32:11 -0300

    create service

--- src/app/services/CreateAppointmentServices.js
+++ src/app/services/CreateAppointmentServices.js
@@ -1,51 +1,13 @@
-import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 import locale from 'date-fns/locale/pt';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment'
-import User from '../models/User';
-import Appointment from '../models/Appointment'
+
 import Notification from '../schemas/Notification';
 
-import File from '../models/File';
-
-import CancelAppointmentServices from '../services/CancelAppointmentServices';
-
-class AppointmentController {
-
-  async index(req, res) {
-    const { page = 1 } = req.query;
-
-    const appointment = await Appointment.findAll({
-      where: { user_id: req.userId, canceled_at: null },
-      order: ['date'],
-      attributes: ['id', 'date', 'past', 'cancelable'],
-      limit: 20,
-      offset: (page - 1) * 20,
-      include: [{
-        model: User,
-        as: 'provider',
-        attributes: ['id', 'name'],
-        include: [{
-          model: File,
-          as: 'avatar',
-          attributes: ['id', 'path', 'url'],
-        }]
-      }]
-    })
-
-    return res.json(appointment);
-  }
-
-  async store(req, res) {
-    // const { provider_id, date } = req.body;
-
-    // const appointment = await CreateAppointmentServices.run({
-    //   provider_id,
-    //   user_id: req.userId,
-    //   date
-    // });
+class CreateAppointmentServices {
+  async run({ provider_id, user_id, date }) {
 
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -97,17 +59,8 @@ class AppointmentController {
       user: provider_id
     });
 
-    return res.json(appointment);
   }
 
-  async delete(req, res) {
-    const appointment = await CancelAppointmentServices.run({
-      provider_id: req.params.id,
-      user_id: req.userId
-    });
-
-    return res.json(appointment);
-  }
 }
 
-export default new AppointmentController();
+export default new CreateAppointmentServices();
