74fc66c59e4a059c129d2bae8affe59024541328
Felps03 <felipe.031293@gmail.com>
2019-10-17 00:07:17 -0300

    new tests

--- __tests__/integration/controllers/user.test.js
+++ __tests__/integration/controllers/user.test.js
@@ -5,6 +5,8 @@ import app from '../../../src/app';
 import truncate from '../../util/truncate';
 import factory from '../../factories';
 
+const TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNTcxMTA3MDAxLCJleHAiOjE1NzE3MTE4MDF9.KC75v7nWX2o9FuqINiBbQP3pW-fnuI4vhII7oAwkioU';
+
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
@@ -36,7 +38,47 @@ describe('User', () => {
     const compareHash = await bcrypt.compare('123456', user.password_hash);
 
     expect(compareHash).toBe(true);
+  });
 
-  })
+  it('should fails user without the email when new user created', async () => {
+    const response = await request(app).post('/user').send({
+      name: 'Felipe',
+      password: "123456"
+    });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should fails user the email is not validated when new user update', async () => {
+    const response = await request(app).put('/user').send({
+      email: "123456"
+    }).set('Authorization', TOKEN);
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should fails if updating an email be the same as an existing user', async () => {
+
+    const user = await factory.attrs('User');
+    await request(app).post('/user').send(user);
+
+    const email = await request(app).post('/user').send({
+      name: "Felipe Santos",
+      email: "felipe@gmail.com",
+      password: "123456"
+    }).then(response => response.body.email);
+
+    const auth = await request(app).post('/sessions')
+      .send({
+        email,
+        password: '123456'
+      }).then(response => response.body);
+
+    const response = await request(app).put('/user').send({
+      email: user.email
+    }).set('Authorization', `Bearer ${auth.token}`);
+
+    expect(response.status).toBe(400);
+  });
 
 });
