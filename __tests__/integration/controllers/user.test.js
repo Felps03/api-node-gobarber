cd4fe4069c0db7546e801cbd1f4c23909a4aa77a
Felipe da Silva dos Santos <felipe.silva.santos@hdi.com.br>
2019-11-22 15:37:45 -0300

    teste

--- __tests__/integration/controllers/user.test.js
+++ __tests__/integration/controllers/user.test.js
@@ -12,7 +12,7 @@ describe('User', () => {
     await truncate();
   });
 
-  it('should be able to register', async () => {
+  it('[SUCCESS] should be able to register', async () => {
     const user = await factory.attrs('User');
 
     const response = await request(app).post('/user').send(user);
@@ -20,7 +20,7 @@ describe('User', () => {
     expect(response.status).toBe(200);
   });
 
-  it('should not be able to register with dublicated email', async () => {
+  it('[FAIL] should not be able to register with dublicated email', async () => {
     const user = await factory.attrs('User');
 
     await request(app).post('/user').send(user);
@@ -30,7 +30,7 @@ describe('User', () => {
     expect(response.status).toBe(400);
   });
 
-  it('should encrypt user password when new user created', async () => {
+  it('[SUCCESS] should encrypt user password when new user created', async () => {
     const user = await factory.create('User', {
       password: "123456"
     });
@@ -40,7 +40,7 @@ describe('User', () => {
     expect(compareHash).toBe(true);
   });
 
-  it('should fails user without the email when new user created', async () => {
+  it('[FAIL] should fails user without the email when new user created', async () => {
     const response = await request(app).post('/user').send({
       name: 'Felipe',
       password: "123456"
@@ -49,59 +49,14 @@ describe('User', () => {
     expect(response.status).toBe(400);
   });
 
-  it('should fails user the email is not validated when new user update', async () => {
+  it('[FAIL] should fails user the email is not validated when new user update', async () => {
     const response = await request(app).put('/user').send({
       email: "123456"
     }).set('Authorization', TOKEN);
-
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(401);
   });
 
-  // it('should fails if updating an email be the same as an existing user', async () => {
-
-  //   const user = await factory.attrs('User');
-  //   await request(app).post('/user').send(user);
-
-  //   const email = await request(app).post('/user').send({
-  //     name: "Felipe Santos",
-  //     email: "felipe@gmail.com",
-  //     password: "123456"
-  //   }).then(response => response.body.email);
-
-  //   const auth = await request(app).post('/sessions')
-  //     .send({
-  //       email,
-  //       password: '123456'
-  //     }).then(response => response.body);
-
-  //   const response = await request(app).put('/user').send({
-  //     email: user.email
-  //   }).set('Authorization', `Bearer ${auth.token}`);
-
-  //   expect(response.status).toBe(400);
-  // });
-
-  // it('should be able to update only the email for user and return all attributes', async () => {
-  //   const email = await request(app).post('/user').send({
-  //     name: "Felipe Santos",
-  //     email: "felipe@gmail.com",
-  //     password: "123456"
-  //   }).then(response => response.body.email);
-
-  //   const auth = await request(app).post('/sessions')
-  //     .send({
-  //       email,
-  //       password: '123456'
-  //     }).then(response => response.body);
-
-  //   const response = await request(app).put('/user').send({
-  //     email: 'felps@gmail.com'
-  //   }).set('Authorization', `Bearer ${auth.token}`);
-
-  //   expect(response.body).toHaveProperty('id', 'name', 'email', 'provider');
-  // });
-
-  it('should fails if updating input in oldPassword different password user', async () => {
+  it('[FAIL] should fails if updating input in oldPassword different password user', async () => {
     const email = await request(app).post('/user').send({
       name: "Felipe Santos",
       email: "felipe@gmail.com",
